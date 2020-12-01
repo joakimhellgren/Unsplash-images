2671a2c9e8e0e9160695c7a8b5129315f4103d60
Joakim Hellgren <hellojoakim@gmail.com>
2020-12-01 11:42:54 +0100

    few refactoring changes, moved around some code blocks

--- diffableimagesearch/ViewController.swift
+++ diffableimagesearch/ViewController.swift
@@ -4,7 +4,7 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
     
     // MARK: Bindings & logic
     var searchController = UISearchController()
-    var statusText = " "
+    
     
     // api
     var dataManager = DataManager()
@@ -18,7 +18,6 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
     var page = 1
     var searchInput: String?
     
-    
     // prevent methods from requesting data when a request is already in progress.
     var prefetchState: PrefetchState = .idle
     enum PrefetchState {
@@ -26,6 +25,7 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
         case idle
     }
     
+    var statusText = " "
     // sets the text of the footer textLabel depending on which case we specify in our collection supplementary view
     var statusLabelText = StatusLabelText.initial
     enum StatusLabelText {
@@ -47,7 +47,6 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
      // MARK: diffable data source
     private lazy var diffableDataSource: UICollectionViewDiffableDataSource<Int, Result> = {
         let dataSource = UICollectionViewDiffableDataSource<Int, Result>(collectionView: collectionView) { collectionView, indexPath, item in
-            
             // cell
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.identifier, for: indexPath) as! CollectionViewCell
             cell.configure(label: self.images[indexPath.row].user.username, image: self.images[indexPath.row].urls.small)
@@ -68,7 +67,6 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
                 fatalError("Element \(kind) not supported")
             }
         }
-        
         return dataSource
     }()
     
@@ -76,15 +74,12 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
      // MARK: Collection view setup
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-       
         layout.itemSize = CGSize(width: 200, height: 200)
         layout.footerReferenceSize = CGSize(width: 0, height: 60)
+        
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.register(CollectionViewCell.self,
-                                forCellWithReuseIdentifier: CollectionViewCell.identifier)
-        collectionView.register(CollectionReusableView.self,
-                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
-                                withReuseIdentifier: "sampleFooterIdentifier")
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.identifier)
+        collectionView.register(CollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "sampleFooterIdentifier")
         return collectionView
     }()
     
@@ -100,8 +95,7 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
         diffableDataSource.apply(snapshot, animatingDifferences: true)
         
         // update footer text / activity indicator
-        if let footer = self.collectionView.supplementaryView(forElementKind: UICollectionView.elementKindSectionFooter,
-                                                              at: IndexPath(item: 0, section: 0)) as? CollectionReusableView {
+        if let footer = self.collectionView.supplementaryView(forElementKind: UICollectionView.elementKindSectionFooter, at: IndexPath(item: 0, section: 0)) as? CollectionReusableView {
             switch statusLabelText {
             case .initial:
                 statusText = "Search for something"
@@ -118,7 +112,67 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
             }
             footer.fill(with: statusText, loading: loadingData)
         }
-        
+    }
+    
+    
+    // performs API requests based on the type request we have specified.
+    // Initial search requests always trigger the case .request.
+    // Pagination (loading more data after our inital request) always trigger the case .paginate
+    private func fetchData(searchTerm: String, page: Int) {
+        self.prefetchState = .fetching
+        self.searchInput = searchTerm
+            switch fetchState {
+            case .request:
+                // simulate longer loading times for demo purposes (+ 1.5 second from the time it is invoked)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                    self.dataManager.fetch(page: page, searchTerm: searchTerm) { images in
+                        if images.results.count != 0 {
+                            self.statusLabelText = .loading
+                            self.images.append(contentsOf: images.results)
+                            self.totalCount = images.total
+                            self.currentCount = images.results.count
+                            self.update(with: self.images)
+                            self.page += 1
+                            // simulate longer loading times for demo purposes (+ 2 second from the time it is invoked)
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                self.prefetchState = .idle
+                            }
+                        }
+                        if self.currentCount == self.totalCount {
+                            self.statusLabelText = .endOfResult
+                            self.update(with: images.results)
+                            // simulate longer loading times for demo purposes (+ 2 second from the time it is invoked)
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                self.prefetchState = .idle
+                            }
+                        }
+                    }
+                }
+            case .paginate:
+                // simulate longer loading times for demo purposes (+ 1.5 second from the time it is invoked)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                    self.dataManager.fetch(page: page, searchTerm: searchTerm) { images in
+                        if !images.results.isEmpty {
+                            self.images.append(contentsOf: images.results)
+                            self.currentCount += images.results.count
+                            self.update(with: self.images)
+                            self.page += 1
+                            // simulate longer loading times for demo purposes (+ 2 second from the time it is invoked)
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                self.prefetchState = .idle
+                            }
+                        }
+                        if self.currentCount == self.totalCount {
+                            self.statusLabelText = .endOfResult
+                            self.update(with: self.images)
+                            // simulate longer loading times for demo purposes (+ 2 second from the time it is invoked)
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                self.prefetchState = .idle
+                            }
+                        }
+                    }
+                }
+            }
     }
     
     
@@ -143,7 +197,7 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
         self.update(with: images)
     }
     
-    // MARK: did select item at
+    
     // Let's user navigate to a new page with more information on the image clicked
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
@@ -152,8 +206,6 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
         view.configure(user: image.user.username, image: image.urls.regular, date: "created: \(image.created_at)", description: image.description ?? "no description available")
         self.navigationController?.pushViewController(view, animated: true)
     }
-    
-
     
 
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
@@ -173,69 +225,6 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
     }
     
     
-    // performs API requests based on the type request we have specified.
-    // Initial search requests always trigger the case .request.
-    // Pagination (loading more data after our inital request) always trigger the case .paginate
-    func fetchData(searchTerm: String, page: Int) {
-        self.prefetchState = .fetching
-        self.searchInput = searchTerm
-            switch fetchState {
-            case .request:
-                
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    self.dataManager.fetch(page: page, searchTerm: searchTerm) { images in
-                        if images.results.count != 0 {
-                            self.statusLabelText = .loading
-                            self.images.append(contentsOf: images.results)
-                            self.totalCount = images.total
-                            self.currentCount = images.results.count
-                            self.update(with: self.images)
-                            self.page += 1
-                            // simulate longer loading times for demo purposes (+ 2 second from the time it is invoked)
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                                self.prefetchState = .idle
-                            }
-                        }
-                        if self.currentCount == self.totalCount {
-                            self.statusLabelText = .endOfResult
-                            self.update(with: images.results)
-                            // simulate longer loading times for demo purposes (+ 2 second from the time it is invoked)
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                                self.prefetchState = .idle
-                            }
-                        }
-                        
-                    }
-                }
-            case .paginate:
-                
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                    self.dataManager.fetch(page: page, searchTerm: searchTerm) { images in
-                        if !images.results.isEmpty {
-                            self.images.append(contentsOf: images.results)
-                            self.currentCount += images.results.count
-                            self.update(with: self.images)
-                            self.page += 1
-                            // simulate longer loading times for demo purposes (+ 2 second from the time it is invoked)
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                                self.prefetchState = .idle
-                            }
-                        }
-                        if self.currentCount == self.totalCount {
-                            self.statusLabelText = .endOfResult
-                            self.update(with: self.images)
-                            // simulate longer loading times for demo purposes (+ 2 second from the time it is invoked)
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                                self.prefetchState = .idle
-                            }
-                        }
-                    }
-                }
-                
-            }
-        
-    }
-    
     // calls .paginate request when user scrolls close to the bottom of the view container.
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard prefetchState == .idle else { return }
@@ -249,10 +238,8 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
                currentCount < totalCount {
                 fetchState = .paginate
                 fetchData(searchTerm: input, page: page)
-                
             }
         }
-        
     }
     
 }
