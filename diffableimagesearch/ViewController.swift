1b00ed15c9247e2a14a66f3e3da1f3f28384fbf8
Joakim Hellgren <hellojoakim@gmail.com>
2021-01-11 16:23:40 +0100

    Added styling to login view and a placeholder for logged in user prompt in home view

--- diffableimagesearch/ViewController.swift
+++ diffableimagesearch/ViewController.swift
@@ -6,6 +6,7 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
     
     var searchController = UISearchController()
     let isUserLoggedIn: Bool = false
+    var user: String?
     var dataManager = DataManager()
     // api data / trackers
     var images: [Result] = []
@@ -31,13 +32,12 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
     
     
     
-    
+
     //MARK: Welcome message configuration
     
     private var showPopupMessage = true
     private let popupBackgroundView: UIView = {
         let popupBackgroundView = UIView()
-        popupBackgroundView.backgroundColor = .systemOrange
         popupBackgroundView.alpha = 0
         return popupBackgroundView
     }()
@@ -46,19 +46,17 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
         popupView.backgroundColor = .white
         popupView.layer.masksToBounds = true
         popupView.layer.cornerRadius = 12
+        popupView.alpha = 0
         return popupView
     }()
     
     func showPopup(with title: String, message: String, on viewController: ViewController) {
         popupBackgroundView.frame = view.bounds
-        
         view.addSubview(popupBackgroundView)
-        let popupViewSize = popupBackgroundView.frame.size.width - 1.125
         
-        popupView.frame = CGRect(x: 0, y: 0, width: popupViewSize, height: popupViewSize + 64)
-        popupView.center = CGPoint(x: view.center.x, y: view.center.y - (view.center.y / 4))
-        //popupView.alpha = 0
-        popupView.backgroundColor = .secondarySystemBackground
+        let popupViewSize = popupBackgroundView.frame.size.width - (popupBackgroundView.frame.size.width / 7.125)
+        popupView.frame = CGRect(x: 0, y: 0, width: popupViewSize, height: popupViewSize + 128)
+        popupView.center = CGPoint(x: view.center.x, y: view.center.y - (view.center.y / 2.5))
         view.addSubview(popupView)
         
         let titleLabel = UILabel(frame: CGRect(x: 16, y: 0, width: popupView.frame.size.width - 32, height: 54))
@@ -67,7 +65,7 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
         titleLabel.textAlignment = .center
         popupView.addSubview(titleLabel)
         
-        let messageLabel = UILabel(frame: CGRect(x: 16, y: 40, width: popupView.frame.size.width - 32, height: 64))
+        let messageLabel = UILabel(frame: CGRect(x: 16, y: 32, width: popupView.frame.size.width - 32, height: 64))
         messageLabel.numberOfLines = 0
         messageLabel.font = .preferredFont(forTextStyle: .subheadline)
         messageLabel.text = message
@@ -78,8 +76,8 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
             let emailField = UITextField()
             emailField.frame = CGRect(x: 16, y: popupView.frame.size.height - 324, width: popupView.frame.size.width - 32, height: 40)
             emailField.placeholder = "Email"
-            let lineView = UIView(frame: CGRect(x: 0, y: emailField.frame.size.height, width: emailField.frame.size.width, height: 0.5))
-            lineView.alpha = 0.75
+            let lineView = UIView(frame: CGRect(x: 0, y: emailField.frame.size.height, width: emailField.frame.size.width, height: 1))
+            lineView.alpha = 0.6
             lineView.backgroundColor = .opaqueSeparator
             emailField.addSubview(lineView)
             return emailField
@@ -91,59 +89,94 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
             passwordField.frame = CGRect(x: 16, y: popupView.frame.size.height - 252, width: popupView.frame.size.width - 32, height: 40)
             passwordField.placeholder = "Password"
             passwordField.isSecureTextEntry = true
-            let lineView = UIView(frame: CGRect(x: 0, y: passwordField.frame.size.height, width: passwordField.frame.size.width, height: 0.5))
-            lineView.alpha = 0.75
+            let lineView = UIView(frame: CGRect(x: 0, y: passwordField.frame.size.height, width: passwordField.frame.size.width, height: 1))
+            lineView.alpha = 0.6
             lineView.backgroundColor = .opaqueSeparator
-            let forgotPass = UIButton(type: .system, primaryAction: UIAction(title: "Forgot password?", handler: { _ in return }))
-            forgotPass.frame = CGRect(x: 0, y: passwordField.frame.size.height + 16, width: passwordField.frame.size.width, height: 20)
-            forgotPass.contentHorizontalAlignment = .trailing
             passwordField.addSubview(lineView)
-            passwordField.addSubview(forgotPass)
             return passwordField
         }()
         popupView.addSubview(passwordField)
         
+        let forgotPass = UIButton(type: .system, primaryAction: UIAction(title: "Forgot password?", handler: { _ in print("forgot password button clicked") }))
+        forgotPass.frame = CGRect(x: 16, y: popupView.frame.size.height - 200, width: popupView.frame.size.width - 32, height: 20)
+        forgotPass.contentHorizontalAlignment = .trailing
+        popupView.addSubview(forgotPass)
+        
         let loginButton = UIButton(type: .system, primaryAction: UIAction(title: "Log in", handler: { _ in self.dismissPopup() }))
         loginButton.frame = CGRect(x: 16, y: popupView.frame.size.height - 128, width: popupView.frame.size.width - 32, height: 48)
-        loginButton.backgroundColor = .systemBackground
+        loginButton.backgroundColor = .secondarySystemFill
+        loginButton.layer.cornerRadius = 12
         popupView.addSubview(loginButton)
         
         let registerButton = UIButton(type: .system, primaryAction: UIAction(title: "Register", handler: { _ in return }))
         registerButton.frame = CGRect(x: 16, y: popupView.frame.size.height - 64, width: popupView.frame.size.width - 32, height: 48)
-        registerButton.backgroundColor = .systemBackground
+        registerButton.backgroundColor = .secondarySystemFill
+        registerButton.layer.cornerRadius = 12
         popupView.addSubview(registerButton)
         
         UIView.animate(withDuration: 0.25, animations: {
             self.popupBackgroundView.alpha = 1
-        }, completion: { done in
-            if done {
-                UIView.animate(withDuration: 0.25, animations: {
-                    
-                    self.popupView.alpha = 1
-                })
-            }
+            self.popupView.alpha = 1
         })
     }
     
     func dismissPopup() {
+        user = "Jane"
+        guard let username = user else { return }
         UIView.animate(withDuration: 0.25, animations: {
-            self.popupBackgroundView.alpha = 0
+            self.popupView.alpha = 0
         }, completion: { done in
+            if done {
+                UIView.animate(withDuration: 0.25, animations: {
+                    self.popupBackgroundView.alpha = 0
+                }, completion: { done in
                     if done {
-                        
                         self.popupView.removeFromSuperview()
                         self.popupBackgroundView.removeFromSuperview()
-                        self.navigationController?.navigationBar.alpha = 1
-                        
                         self.collectionView.bounces = true
+                        self.addWelcomeMessage(with: username, on: self)
+                        UIView.animate(withDuration: 0.25, animations: {
+                            self.navigationController?.navigationBar.alpha = 1
+                        })
                     }
-                
+                })
             }
+        }
         )
         searchController.searchBar.isUserInteractionEnabled = true
         
     }
     
+    
+    private let userView: UIView = {
+        let userView = UIView()
+        userView.backgroundColor = .white
+        userView.layer.masksToBounds = true
+        userView.layer.cornerRadius = 12
+        userView.alpha = 0
+        return userView
+    }()
+    
+    
+    func addWelcomeMessage(with username: String, on viewController: ViewController) {
+        userView.frame = CGRect(x: 16, y: 0, width: view.frame.width, height: view.frame.height - 32)
+        userView.center = CGPoint(x: view.center.x, y: view.center.y)
+        view.addSubview(userView)
+        
+        let usernameLabel = UILabel(frame: CGRect(x: 16, y: 128, width: view.frame.size.width - 32, height: 54))
+        usernameLabel.text = "Welcome back, \(username)."
+        usernameLabel.font = .preferredFont(forTextStyle:  .title3)
+        usernameLabel.textAlignment = .center
+        userView.addSubview(usernameLabel)
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            self.userView.alpha = 1
+        })
+    }
+    
+    func dissmissWelcomeMessage() {
+        self.userView.removeFromSuperview()
+    }
     
     
     
@@ -249,7 +282,7 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
         return collectionView
     }()
     
-    
+
     
     
     // MARK: other
@@ -289,6 +322,7 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
     //MARK: Searchbar configuration
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         resetDataForNewSearch()
+        dissmissWelcomeMessage()
         if let input = self.searchController.searchBar.text {
             title = "Searching for: \(input)"
             fetchData(searchTerm: input, page: page)
@@ -309,14 +343,12 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
         searchController.searchBar.placeholder = "Search.."
         searchController.searchBar.delegate = self
         navigationItem.searchController = searchController
-        //title = ""
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         if showPopupMessage == true {
-           
             collectionView.bounces = false
             navigationController?.navigationBar.alpha = 0
             searchController.searchBar.isUserInteractionEnabled = false
@@ -332,7 +364,7 @@ class ViewController : UIViewController, UISearchBarDelegate, UICollectionViewDe
                     "Splashify"
                 ]
                 guard let randomAppName = RGAppNames.randomElement() else { return }
-                showPopup(with: "Hi there, stranger.", message: "Welcome to \(randomAppName), please log in to continue.", on: self)
+                showPopup(with: "Hello, stranger.", message: "Welcome to \(randomAppName), log in to continue.", on: self)
             case true:
                 return
             }
